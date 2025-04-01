5f896df901752f25375cb3e5ee317bd276139b83
D <duytk.job@gmail.com>
2025-03-31 22:48:35 -0400

    Add virtual network and subnet

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -6,3 +6,17 @@ resource "azurerm_resource_group" "rg" {
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "lab12-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "lab12-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
