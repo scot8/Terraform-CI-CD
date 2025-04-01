e78903f996f66a0be889e6f60a0fddfc74438765
D <duytk.job@gmail.com>
2025-03-31 22:05:36 -0400

    update main for lab12

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -8,3 +8,16 @@ output "resource_group_name" {
 }
 
 
+resource "azurerm_virtual_network" "vnet" {
+  name                = "lab12-vnet"
+  location            = azurerm_resource_group.app_rg.location
+  resource_group_name = azurerm_resource_group.app_rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+ 
+resource "azurerm_subnet" "subnet" {
+  name                 = "lab12-subnet"
+  resource_group_name  = azurerm_resource_group.app_rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
